c3f5fdad9dc49b9138cad449ca13797c0bbbc9ec
Lex Xiong <lex.xiong@bandins.com>
2024-10-11 19:12:00 +0800

    新增OHC模块

---
+++ ohc-module/ohc-module-activity-api/src/main/java/com/ourhomechina/activity/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * activity 模块，我们放活动业务。
+ * 例如说：活动发布、活动报名、活动核销等等
+ *
+ * 1. Controller URL：以 /activity/ 开头，避免和其它 Module 冲突
+ * 2. DataObject 表名：以 activity_ 开头，方便在数据库中区分
+ */
+package com.ourhomechina.activity;
